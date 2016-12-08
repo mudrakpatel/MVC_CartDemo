95d49c4240c877c2ec98ec932635d42f3e514143
mudrakpatel <mudrakpatel96@gmail.com>
2016-12-07 22:41:11 -0600

    Add project files.

---
+++ MVC_CartDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_CartDemo.Startup))]
+namespace MVC_CartDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
